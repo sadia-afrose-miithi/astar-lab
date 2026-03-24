1358ec348fb710a3d2df98973b0278e7fb2fdf8e
Most.Sadia Afrose Mithi <sadiaafrosemithi00@gmail.com>
2026-03-24 23:25:39 +0600

    Add initial A* algorithm code
    Add A* algorithm implementation

---
+++ astar.py
@@ -0,0 +1,80 @@
+import heapq
+
+# Manhattan distance
+def h(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+
+def astar(grid, start, goal):
+    pq = []
+    heapq.heappush(pq, (0, start))
+
+    visited = set()
+    parent = {}
+    cost = {start: 0}
+
+    while pq:
+        _, current = heapq.heappop(pq)
+
+        if current == goal:
+            path = []
+            node = current
+
+            while node in parent:
+                path.append(node)
+                node = parent[node]
+
+            path.append(start)
+            path.reverse()
+
+            print("Path found with cost", len(path) - 1, "using A*")
+
+            
+            formatted = "[" + ", ".join(f"({x},{y})" for x, y in path) + "]"
+            print("Shortest Path:", formatted)
+            return
+
+        visited.add(current)
+
+        for dx, dy in [(0,1),(1,0),(0,-1),(-1,0)]:
+            nx = current[0] + dx
+            ny = current[1] + dy
+
+            if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]):
+                if grid[nx][ny] == 0 and (nx, ny) not in visited:
+
+                    new_cost = cost[current] + 1
+
+                    if (nx, ny) not in cost or new_cost < cost[(nx, ny)]:
+                        cost[(nx, ny)] = new_cost
+                        f = new_cost + h((nx, ny), goal)
+
+                        heapq.heappush(pq, (f, (nx, ny)))
+                        parent[(nx, ny)] = current
+
+    print("Path not found using A*")
+
+
+
+try:
+    with open("input.txt") as f:
+        lines = f.read().strip().split("\n")
+
+    i = 0
+    r, c = map(int, lines[i].split())
+    i += 1
+
+    grid = []
+    for _ in range(r):
+        grid.append(list(map(int, lines[i].split())))
+        i += 1
+
+    sr, sc = map(int, lines[i].split())
+    i += 1
+
+    tr, tc = map(int, lines[i].split())
+
+    astar(grid, (sr, sc), (tr, tc))
+
+except:
+    print("Input format error")
